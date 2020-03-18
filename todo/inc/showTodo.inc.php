ad2288c20ffeac3e5bb1396a472d492751076e5e
CarixoHD <36801999+CarixoHD@users.noreply.github.com>
2020-03-18 16:33:25 +0100

    Add files via upload

--- todo/inc/showTodo.inc.php
+++ todo/inc/showTodo.inc.php
@@ -1,13 +1,15 @@
 <?php
     include '../inc/db.inc.php';
-    // echo "<ul>";
-    $sql = "SELECT * FROM todo";
-    $result = $con -> query($sql);
-    if($result -> num_rows > 0){
+    date_default_timezone_set("Europe/Oslo");
 
+    function delete($id, $con){
+        $sql = "DELETE FROM todo WHERE id = $id";
+        $result = $con -> query($sql);
+    }
+    function show($result, $con){
       while($row = $result -> fetch_assoc()){
         $id = $row['id'];
-        $text = $row['text'];
+        $text = $row['title'];
         $status = $row['status'];
         if($status != 0){
           $checked = "checked";
@@ -17,16 +19,52 @@
           $checked = "";
           $style = "";
         }
-        echo "<li id=\"entity$id\">";
-        echo "<input type=\"checkbox\" id=\"item$id\" onclick=\"checkMark($id)\" $checked autocomplete=\"off\">";
-        echo "<label id=\"label$id\" for=\"item$id\" $style>$text</label><br>";
-        echo "<button id=\"del$id\" onclick=\"delItem($id)\">Slett</button>";
-        echo "</li>";
+        if ($row['time'] != null){
+          $time = $row['time'];
+          $end = new DateTime($time);
+          $now = new DateTime();
+          $endTime = (int)($end -> modify("+1 day")) -> format("dmYHi");
+          $nowTime = (int)($now -> format("dmYHi"));
+          if($nowTime >= $endTime){
+            $delete = true;
+            delete($id, $con);
+          }
+          else{
+            $delete = false;
+          }
+        }
+        else{
+          $delete = false;
+        }
+
+        if($row == null || $delete == false){
+          echo "<li id=\"entity$id\">";
+          echo "<section class=\"wrapper\">";
+          echo "<input type=\"checkbox\" class=\"checkbox\" id=\"item$id\" onclick=\"checkMark($id)\" $checked autocomplete=\"off\">";
+          echo "<label class= \"text\" id=\"label$id\" for=\"item$id\" $style>$text</label><br>";
+          echo "</section>";
+          echo "<button class=\"delButton\" id=\"del$id\" onclick=\"delItem($id)\">&times;</button>";
+          echo "</li>";
+        }
       }
     }
-    else{
-      printf("Listen er visst tom. Du har enten gjort alt allerede, eller så er det fortsatt ting du kan legge til. ");
+    function main($con, $userID, $familyID){
+      // Check if event is private or public for family
+      if($familyID == 0){
+        $sql = "SELECT * FROM todo WHERE user_id = $userID AND family_id = $familyID ORDER BY time DESC";
+      }
+      else{
+        $sql = "SELECT * FROM todo WHERE family_id = $familyID ORDER BY time DESC";
+      }
+
+      $result = $con -> query($sql);
+      if ($result -> num_rows > 0){
+        show($result, $con);
+      }
+      else{
+        echo "Listen er visst tom. Du har enten gjort alt allerede, eller så er det fortsatt ting du kan legge til. ";
+      }
     }
 
-
+    main($con, $userID, $familyID);
  ?>
