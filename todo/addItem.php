006c897396329255a5010a5a749bafbe148a3842
CarixoHD <36801999+CarixoHD@users.noreply.github.com>
2020-03-18 18:10:03 +0100

    Update addItem.php

--- todo/addItem.php
+++ todo/addItem.php
@@ -5,10 +5,6 @@
       $familyID = $_GET['family'];
       $userID = $_GET['user'];
       $sql = "INSERT INTO todo(title, time, user_id, family_id) VALUES($todo, null, $userID, $familyID)";
-
-      if ($con->query($sql) === FALSE) {
-        echo "Error: " . $sql . "<br>" . $con->error;
-  ***REMOVED***
 ***REMOVED***
     else if (isset($_GET['id']) && isset($_GET['s'])){
       $id = $_GET['id'];
@@ -19,16 +15,12 @@
       else{
         $sql = "UPDATE todo SET time = null, status = $status WHERE id = $id";
   ***REMOVED***
-      if ($con->query($sql) === FALSE) {
-        echo "Error: " . $sql . "<br>" . $con->error;
-  ***REMOVED***
-
 ***REMOVED***
     else {
       echo "An error occured";
 ***REMOVED***
-
-
-
+    if ($con->query($sql) === FALSE) {
+        echo "Error: " . $sql . "<br>" . $con->error;
+***REMOVED***
     $con->close();
 ***REMOVED***
