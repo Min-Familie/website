fc96cf28eea7fd89fd5e72674ff06284eafabccf
Bendik Skarpnes <42250582+BendikSkarpnes@users.noreply.github.com>
2020-03-28 17:32:45 +0100

    Add files via upload

--- js/sidebar.js
+++ js/sidebar.js
@@ -2,11 +2,11 @@ var open = false;
 
 function openNav() {
     if (!open) {
-        document.getElementById("sidebar").style.width = "200px";
-        document.getElementById("main").style.marginLeft = "200px";
+        document.getElementById("sidebar").style.width = "225px";
+        document.getElementById("main").style.marginLeft = "225px";
         open = true;
     }
-
+    
     else {
         document.getElementById("sidebar").style.width = "0";
         document.getElementById("main").style.marginLeft = "0";
