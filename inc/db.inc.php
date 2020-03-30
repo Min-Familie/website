917ad2c6364edddbd0aa0cc65142b69343f42b36
CarixoHD <36801999+CarixoHD@users.noreply.github.com>
2020-03-30 13:01:57 +0200

    Update db.inc.php

--- inc/db.inc.php
+++ inc/db.inc.php
@@ -1,5 +1,5 @@
 <?php
-    $con = new mysqli("localhost", "root", "ShayKing80", "project");
+    $con = new mysqli("localhost", "root", "", "project");
     if($con->connect_error){
       die('Could not connect');
     }
