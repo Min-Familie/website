420008e62b44394165bea7dd2c5c238ab0a7cd97
Bendik Skarpnes <42250582+BendikSkarpnes@users.noreply.github.com>
2020-04-01 13:05:56 +0200

    Add files via upload

--- calendar/publicDay.php
+++ calendar/publicDay.php
@@ -1,58 +1,87 @@
 <?php
-    $user_id = 1;
+    $user_id = 2;
     require "../inc/db.inc.php";
 
     // tidssone
     date_default_timezone_set("Europe/Oslo");
+    
+    // input dag
+    if (isset($_GET["day"])) {$inputDay = $_GET["day"];}
+    else                     {$inputDay = date("Y-m-d");}
 
-    // input måned
-    if (isset($_GET["month"])) {$inputMonth = $_GET["month"];}
-    else                       {$inputMonth = date("Y-m");}
-    // i forhold til inputMonth
-    $month = date("m", strtotime($inputMonth));
-    $year = date("Y", strtotime($inputMonth));
-    $yearMonth = date("Y-m", strtotime($inputMonth));
-    $nextMonth = date("Y-m", strtotime($inputMonth." +1 month"));
-    $prevMonth = date("Y-m", strtotime($inputMonth." -1 month"));
+    function getFamily($con, $user_id) {
+        $family = [];
 
-    function getEvents($con, $user_id, $inputMonth, $prevMonth, $nextMonth) { // slå alle sammen og order by day
+        // navn på alle familiene som personen er med i
+        $sql = "SELECT DISTINCT f.family_name, f.id
+                FROM families f
+                JOIN memberships m 
+                ON f.id = m.family_id
+                WHERE m.family_id IN
+                (
+                    SELECT m1.family_id
+                    FROM memberships m1
+                    WHERE m1.user_id = $user_id
+                )
+                
+
+                
+                UNION
+                /*alle familiemedldmmer i alle familier som personen er med i*/
+                SELECT DISTINCT u.username, u.id
+                FROM users u
+                JOIN memberships m 
+                ON u.id = m.user_id
+                WHERE m.family_id IN
+                (
+                    SELECT m1.family_id
+                    FROM memberships m1
+                    WHERE m1.user_id = $user_id
+                );";
+                
+        $result = $con -> query($sql);
+        while($row = $result -> fetch_assoc()){
+            array_push($family, $row["family_name"]);
+        }
+        return $family;
+    }
+    
+    function getEvents($con, $user_id, $inputDay) {
         $events = [];
 
         // private events fra personen
-        $sql = "SELECT c.id, title, location, day, startHour, startMinute, duration, user_id, family_id, private, username 
+        $sql = "SELECT c.id, title, location, day, startHour, startMinute, duration, user_id, family_id, private, username
                 FROM calendarEvents c
                 JOIN users u
                 ON c.user_id = u.id
                 WHERE user_id = $user_id
                 AND private
-                AND SUBSTRING(day, 1, 7) = '$inputMonth'
-                OR  SUBSTRING(day, 1, 7) = '$prevMonth'
-                OR  SUBSTRING(day, 1, 7) = '$nextMonth'
+                AND day = '$inputDay'
+
 
 
                 UNION
                 /*public events til alle familiemedlemmer i alle familier personen er med i*/
                 SELECT c.id, title, location, day, startHour, startMinute, duration, user_id, family_id, private, username
                 FROM calendarEvents c
-                JOIN users u
+                JOIN users u /*for å få navn, og ikke bare id fra calendar-tabellen*/
                 ON c.user_id = u.id
-                WHERE c.user_id in 
-                (
+                WHERE c.user_id in
+                    ( /*personene som er med i alle disse familiene*/
                     SELECT u.id
                     FROM users u
                     JOIN memberships m 
                     ON u.id = m.user_id
                     WHERE m.family_id IN
-                    (
+                    ( /*familiene personen er med i*/
                         SELECT m1.family_id
                         FROM memberships m1
                         WHERE m1.user_id = $user_id
                     )
                 )
-                AND NOT private
-                AND SUBSTRING(day, 1, 7) = '$inputMonth'
-                OR  SUBSTRING(day, 1, 7) = '$prevMonth'
-                OR  SUBSTRING(day, 1, 7) = '$nextMonth'
+                AND NOT private /*filtrering*/
+                AND day = '$inputDay'
+
 
 
                 UNION
@@ -74,11 +103,7 @@
                         WHERE m1.user_id = $user_id
                     )
                 )
-                AND SUBSTRING(day, 1, 7) = '$inputMonth'
-                OR  SUBSTRING(day, 1, 7) = '$prevMonth'
-                OR  SUBSTRING(day, 1, 7) = '$nextMonth'
-                
-                ORDER BY day, startHour, startMinute;";
+                AND day = '$inputDay';";
         
         $result = $con -> query($sql);
         while($row = $result -> fetch_assoc()){
@@ -98,8 +123,11 @@
         return $events;
     }   
 
+
+    // familiemedlemmer fra db
+    $family = getFamily($con, $user_id);
     // events fra db
-    $events = getEvents($con, $user_id, $inputMonth, $prevMonth, $nextMonth);
+    $events = getEvents($con, $user_id, $inputDay);
 ?>
 
 <!DOCTYPE html>
@@ -107,7 +135,7 @@
     <head>
         <title>Kalender - Dag</title>
         <meta charset="utf-8">
-        <link rel="stylesheet" type="text/css"  href="../css/calendarMonth.css">
+        <link rel="stylesheet" type="text/css"  href="../css/calendarDay.css">
         <link rel="stylesheet" type="text/css"  href="../css/visuals.css">
         <link rel="icon"       type="image/png" href="../visuals/logo.png">
     </head>
@@ -115,12 +143,12 @@
         <?php
             echo "<article>";
             include "../visuals/header.html";
-            require "../inc/calendarMonth.inc.php"; 
+            require "../inc/calendarDay.inc.php"; 
             echo "</article>";
             include "../visuals/footer.html";
         ?>
-        </section>
         <script type="text/javascript" src="../js/sidebar.js"></script>
+        </section>
     </body>
 </html>
 
