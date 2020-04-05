ca73c024a7e9a1dd9fa582bea0da634ed4f03175
Bendik Skarpnes <42250582+BendikSkarpnes@users.noreply.github.com>
2020-04-05 23:18:51 +0200

    Add files via upload

--- js/sidebar.js
+++ js/sidebar.js
@@ -2,12 +2,33 @@ var open = false;
 
 function openNav() {
     if (!open) {
-        document.getElementById("sidebar").style.width = "225px";
-        document.getElementById("main").style.marginLeft = "225px";
+        if (window.innerWidth <= 800) {
+            document.getElementById("sidebar").style.height = "253px";
+            document.getElementById("sidebar").style.width = "100%";
+            document.getElementById("main").style.marginLeft = "0";
+        }
+        else {
+            document.getElementById("sidebar").style.height = "100%";
+            document.getElementById("sidebar").style.width = "225px";
+            document.getElementById("main").style.marginLeft = "225px";
+        }
         open = true;
     }
     
     else {
+        /*
+        if (window.innerWidth <= 800) {
+            document.getElementById("sidebar").style.height = "0";
+            document.getElementById("sidebar").style.width = "100%";
+            document.getElementById("main").style.marginLeft = "0";
+        }
+        else {
+            document.getElementById("sidebar").style.height = "100%";
+            document.getElementById("sidebar").style.width = "0";
+            document.getElementById("main").style.marginLeft = "0";
+        }
+        */
+        document.getElementById("sidebar").style.height = "0";
         document.getElementById("sidebar").style.width = "0";
         document.getElementById("main").style.marginLeft = "0";
         open = false;
