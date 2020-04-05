a0f18d7b2b22c6c4d22e8ca1f4fec8ef8b7693eb
CarixoHD <shayking7yt@gmail.com>
2020-04-05 17:03:02 +0200

    todo css changes

--- todo/todo.php
+++ todo/todo.php
@@ -18,7 +18,7 @@
                     SELECT m1.family_id
                     FROM memberships m1
                     WHERE m1.user_id = $user
-                );";
+                )";
 
         $result = $con -> query($sql);
         while($row = $result -> fetch_assoc()){
@@ -39,41 +39,36 @@
         <link rel="stylesheet" href="../css/visuals.css">
         <title>Todo</title>
     </head>
-    <body>
+    <body class="todo">
         <script src="../js/todo.js"></script>
         <script src="../js/checkmark.js"></script>
         <script src="../js/checkempty.js"></script>
         <script src="../js/sidebar.js">
 
         </script>
-        <?php include '../visuals/header.html'; ?>
-        <main>
-        <section class="leggInn">
-            <h1>HUSKELISTE</h1>
-            <!-- <section role="status" aria-live="polite">
-            </section> -->
+            <?php include '../visuals/header.html'; ?>
+        <main class="todoMain">
+            <h1 class="todoTitle">HUSKELISTE</h1>
+                <form id="nyItemForm" onsubmit="return false">
+                    <input type="text" id="nyItem" placeholder="E.g. Støvsuge huset...">
+                    <select id="permission">
+                        <option value=0>Privat</option>
+                        <?php
+                            foreach ($families as $surname) { //surname=[navn, id]
 
-            <form id="nyItemForm" onsubmit="return false">
-                <input type="text" id="nyItem" placeholder="E.g. Støvsuge huset...">
-                <select id="permission">
-                    <option value=0>Privat</option>
-                    <?php
-                        foreach ($families as $surname) { //surname=[navn, id]
-
-                            echo "<option value=\"$surname[1]\">$surname[0]</option>";
-                        }
-                    ?>
-                </select>
-                <button type="button" id="nyButton" name="button" onclick="if(checkEmpty()){onClick();}">Legg til</button>
-                <input type="hidden" id="user" value="<?php echo $user; ?>">
-            </form>
-            <p id="errorMessage"></p>
-        </section>
-        <section class="items">
-        <ul id="entries">
-            <?php include '../inc/showTodo.inc.php'; ?>
-        </ul>
-        </section>
+                                echo "<option value=\"$surname[1]\">$surname[0]</option>";
+                            }
+                        ?>
+                    </select>
+                    <button type="button" id="nyButton" name="button" onclick="if(checkEmpty()){onClick();}">Legg til</button>
+                    <input type="hidden" id="user" value="<?php echo $user; ?>">
+                </form>
+                <p id="errorMessage"></p>
+                <section class="items">
+                    <ul id="entries">
+                        <?php include '../inc/showTodo.inc.php'; ?>
+                    </ul>
+                </section>
         </main>
         <?php include '../visuals/footer.html'; ?>
     </body>
