0cb1f90bd17513b72bd0a391fe2daf88c213274f
CarixoHD <shayking7yt@gmail.com>
2020-04-06 19:21:31 +0200

    menu

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,36 +1,53 @@
 var open = false;
 
 function openNav() {
-    if (!open) {
-        if (window.innerWidth <= 800) {
-            document.getElementById("sidebar").style.height = "253px";
-            document.getElementById("sidebar").style.width = "100%";
-            document.getElementById("main").style.marginLeft = "0";
-        }
-        else {
-            document.getElementById("sidebar").style.height = "100%";
-            document.getElementById("sidebar").style.width = "225px";
+    // if (!open) {
+        // if (window.innerWidth <= 800) {
+        //     document.getElementById("sidebar").style.height = "253px";
+        //     document.getElementById("sidebar").style.width = "100%";
+        //     document.getElementById("main").style.marginLeft = "0";
+        // }
+        // else {
+        //     document.getElementById("sidebar").style.height = "100%";
+        //     document.getElementById("sidebar").style.width = "225px";
+        //     document.getElementById("main").style.marginLeft = "225px";
+        // }
+        // open = true;
+    let nav = document.getElementById('sidebar');
+    if (window.innerWidth <= 800) {
+        nav.classList.toggle('sidebar_toggled_mobile');
+        document.getElementById("main").style.marginLeft = "0";
+    }
+    else {
+        nav.classList.toggle('sidebar_toggled');
+        if (nav.classList.contains('sidebar_toggled')) {
             document.getElementById("main").style.marginLeft = "225px";
         }
-        open = true;
-    }
-    
-    else {
-        /*
-        if (window.innerWidth <= 800) {
-            document.getElementById("sidebar").style.height = "0";
-            document.getElementById("sidebar").style.width = "100%";
+        else{
             document.getElementById("main").style.marginLeft = "0";
         }
-        else {
-            document.getElementById("sidebar").style.height = "100%";
-            document.getElementById("sidebar").style.width = "0";
-            document.getElementById("main").style.marginLeft = "0";
-        }
-        */
-        document.getElementById("sidebar").style.height = "0";
-        document.getElementById("sidebar").style.width = "0";
-        document.getElementById("main").style.marginLeft = "0";
-        open = false;
     }
+
+
+
+    // }
+    //
+    // else {
+    //     /*
+    //     if (window.innerWidth <= 800) {
+    //         document.getElementById("sidebar").style.height = "0";
+    //         document.getElementById("sidebar").style.width = "100%";
+    //         document.getElementById("main").style.marginLeft = "0";
+    //     }
+    //     else {
+    //         document.getElementById("sidebar").style.height = "100%";
+    //         document.getElementById("sidebar").style.width = "0";
+    //         document.getElementById("main").style.marginLeft = "0";
+    //     }
+    //     */
+    //     document.getElementById("sidebar").style.height = "0";
+    //     document.getElementById("sidebar").style.width = "0";
+    //     document.getElementById("main").style.marginLeft = "0";
+    //     open = false;
+    // }
 }
