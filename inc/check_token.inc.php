4e2a3161a56c7010d3a4671d8e4503a662f2abc5
Bendik Skarpnes <42250582+BendikSkarpnes@users.noreply.github.com>
2020-05-02 18:08:38 +0200

    Add files via upload

---
+++ inc/check_token.inc.php
@@ -0,0 +1,26 @@
+<?php 
+    // require_once '../vendor/autoload.php';
+
+    require $_SERVER['DOCUMENT_ROOT'] . '/minfamilie/inc/db.inc.php';
+
+    $token = $_GET['token'];
+    /*
+    $client = new Google_Client(['client_id' => '280130080722-8ruhprpjaqt1vorhd8s245l9gtqgmr61.apps.googleusercontent.com']);  // Specify the CLIENT_ID of the app that accesses the backend
+    $payload = $client->verifyIdToken($token);
+    
+    if ($payload) {
+        $userid = $payload['sub'];
+        $sql = "INSERT INTO users (username, forename, surename, password) VALUES(\"$userid\", \"test\", \"test\", \"test\");";
+        if(!$result = $con -> query($sql)){
+            echo "wrong";
+        }
+  
+    // If request specified a G Suite domain:
+    //$domain = $payload['hd'];
+    } 
+    else {
+        echo "no";
+    }
+    */
+    //echo $sql;
+?>
