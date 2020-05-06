b62a2438456da92aec22b8ab6b2024371dc63935
CarixoHD <shayking7yt@gmail.com>
2020-05-06 16:19:06 +0200

    fixed deleted files

---
+++ login.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+        <meta charset="utf-8">
+        <meta name="google-signin-client_id" content="280130080722-8ruhprpjaqt1vorhd8s245l9gtqgmr61.apps.googleusercontent.com">
+        <link rel="stylesheet" type="text/css"  href="style.css">
+        <link rel="icon"       type="image/png" href="favicon.png">
+    </head>
+    <body>
+        <div class="g-signin2" data-onsuccess="onSignIn"></div>
+
+        <script src="js/login.js"></script>
+        <script src="https://apis.google.com/js/platform.js" async defer></script>
+    </body>
+</html>
