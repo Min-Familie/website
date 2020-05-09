a44f84ce6eb11533a91bbf9073e5dbb9377ea826
BendikSkarpnes <bendik.skarpnes@gmail.com>
2020-05-09 11:09:07 +0200

    standarisation: header, footer, ...

--- login.php
+++ login.php
@@ -3,12 +3,17 @@
     <head>
         <title></title>
         <meta charset="utf-8">
-        <meta name="google-signin-client_id" content="280130080722-8ruhprpjaqt1vorhd8s245l9gtqgmr61.apps.googleusercontent.com">
-        <link rel="stylesheet" type="text/css"  href="style.css">
-        <link rel="icon"       type="image/png" href="favicon.png">
+        <meta name="google-signin-client_id"    content="280130080722-8ruhprpjaqt1vorhd8s245l9gtqgmr61.apps.googleusercontent.com">
+        <link rel="stylesheet" type="text/css"  href="css/visuals.css">
+        <link rel="stylesheet" type="text/css"  href="css/login.css">
+        <link rel="icon"       type="image/png" href="visuals/logo.png">
     </head>
     <body>
-        <div class="g-signin2" data-onsuccess="onSignIn"></div>
+        <?php include "visuals/header.php"; ?>
+        <main>
+            <div class="g-signin2" data-onsuccess="onSignIn"></div>
+        </main>
+        <?php include "visuals/footer.html"; ?>
 
         <script src="js/login.js"></script>
         <script src="https://apis.google.com/js/platform.js" async defer></script>
