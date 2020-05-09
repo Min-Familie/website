d3c7f95d56fddb7cea1d94949a1ef6aba68eee5f
Bendik Skarpnes <42250582+BendikSkarpnes@users.noreply.github.com>
2020-05-09 16:40:13 +0200

    Update shopping.php

--- shopping/shopping.php
+++ shopping/shopping.php
@@ -1,5 +1,8 @@
 <?php
     session_start();
+    if (!isset($_SESSION['id'])) {
+        header("Location: ../login.html");
+    }
     if (isset($_SESSION['family_id'])) {
         $family = $_SESSION['family_id'];
     }
